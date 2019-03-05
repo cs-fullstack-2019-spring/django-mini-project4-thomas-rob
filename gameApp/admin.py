3f96ab8af6631d356d694277479fb156831bfaef
tdude0175 <tdude0175@gmail.com>
2019-03-05 11:36:14 -0600

    worked on login page, edited settings

--- gameApp/admin.py
+++ gameApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .forms import GameCollectorForm , GameCollectorModel , GameForm , GameModel
 # Register your models here.
+
+
+admin.site.register(GameCollectorModel)
+admin.site.register(GameModel)
