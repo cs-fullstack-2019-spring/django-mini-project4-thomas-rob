b0d530e8249204c7945295a055c68d6e1c830f91
tdude0175 <tdude0175@gmail.com>
2019-03-05 11:07:32 -0600

    completed basic views for new user form and more detail for index page

--- gameApp/views.py
+++ gameApp/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect    # pulling redirect
 from django.http import HttpResponse
 # Create your views here.
-from .forms import GameModel,GameCollectorModel,GameForm,GameCollectorForm
+from .forms import GameModel,GameCollectorModel,GameForm,GameCollectorForm  #called all forms in models in single line
 
-def index(request):
-    gameList = GameModel.objects.all()
+def index(request):  #for the rendering of the index page
+    gameList = GameModel.objects.all()  # this collects all games made
+    gameCollector = GameCollectorModel.objects.filter(userIDkey =request.user)  # this gets current logged in user
 
     context= \
         {
-            'gameList':gameList
+            'user':gameCollector,  #this converts the gamecollector to user
+            'gameList':gameList   # this addes completed game list that will later filter out based on logged in user
         }
-    return render(request,'gameApp/index.html',context)
+    return render(request,'gameApp/index.html',context)  # this renders the page and start at index
+
+
+def newUser(request):   #for adding a new user
+    userForm = GameCollectorForm()   #collects the form necessary to make a new user
+
+    context = \
+        {
+            'form':userForm  # gets the form to add new user and uses an easy to read name
+        }
+    return render(request,'gameApp/newUser.html',context)
